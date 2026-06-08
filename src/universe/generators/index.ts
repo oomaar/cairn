c555d10261f5ff7eae3fec6ed543cebb4c50105e
oomaar <omar16694@gmail.com>
2026-06-08 14:02:11 +0300

    Add index file to export generator functions for expeditions

---
+++ src/universe/generators/index.ts
@@ -0,0 +1,5 @@
+export { generatePoolPeople } from "./people.gen";
+export { generateRoutes } from "./routes.gen";
+export { generateAssignments, countPoolSeats } from "./assignments.gen";
+export { generateGearAllocations } from "./gear.gen";
+export { generateLogs } from "./logs.gen";
