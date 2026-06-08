f0d68debb141025f162971948de66ea2f0f8b5e6
oomaar <omar16694@gmail.com>
2026-06-08 14:02:57 +0300

    Add FIRST_NAMES and LAST_NAMES constants for participant name generation

---
+++ src/universe/constants/name-pool.ts
@@ -0,0 +1,17 @@
+/** Believable, internationally varied name pools used to deterministically
+ *  generate the wider participant roster. */
+
+export const FIRST_NAMES: readonly string[] = [
+  "Aiko", "Bjorn", "Camila", "Davit", "Elif", "Finn", "Greta", "Hassan",
+  "Imani", "Jonas", "Katya", "Liam", "Maya", "Nikolai", "Olivia", "Pablo",
+  "Qadir", "Rosa", "Sven", "Tara", "Uma", "Viktor", "Wren", "Xiomara",
+  "Yara", "Zane", "Aroha", "Beck", "Cyrus", "Dalia", "Esa", "Freya",
+  "Idris", "Lena", "Mateo", "Noor", "Otto", "Ravi", "Suvi", "Theo",
+];
+
+export const LAST_NAMES: readonly string[] = [
+  "Andersen", "Bauer", "Costa", "Delgado", "Eriksson", "Fontaine", "Gupta",
+  "Haddad", "Ibrahim", "Jansen", "Kowalski", "Larsen", "Moreau", "Nakamura",
+  "Olsen", "Petrov", "Quintero", "Rossi", "Sato", "Tan", "Ueda", "Virtanen",
+  "Walsh", "Xu", "Yilmaz", "Zhang", "Bianchi", "Cohen", "Dubois", "Ferreira",
+];
