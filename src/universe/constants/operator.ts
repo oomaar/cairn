2b9ed3ce1e3b89bd31e2aa17c6414461766da7b9
oomaar <omar16694@gmail.com>
2026-06-08 14:03:02 +0300

    Add OPERATOR constant representing the fictional operator in the universe

---
+++ src/universe/constants/operator.ts
@@ -0,0 +1,8 @@
+import type { Operator } from "../types";
+
+/** The fictional operator the universe is built around. */
+export const OPERATOR: Operator = {
+  id: "northwind",
+  name: "Northwind Expeditions",
+  hq: "Santiago, Chile",
+};
