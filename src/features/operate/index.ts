486ab4d5a63c33d88124218c8807ca0dbf67fc33
oomaar <omar16694@gmail.com>
2026-06-20 13:15:36 +0300

    feat(operate): add index files for LiveOperations, CommandCenter, RiskCenter, EquipmentTracking, and WeatherAlertsCenter

---
+++ src/features/operate/index.ts
@@ -0,0 +1,5 @@
+export { LiveOperations, useLiveExpeditionUpdates } from "./live";
+export { CommandCenter } from "./command/components/command-center";
+export { RiskCenter } from "./risk/components/risk-center";
+export { EquipmentTracking } from "./equipment/components/equipment-tracking";
+export { WeatherAlertsCenter } from "./weather";
