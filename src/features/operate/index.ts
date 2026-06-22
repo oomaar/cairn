e44ef3b788cceafba9f832fb7f99ec438499d4a7
oomaar <omar16694@gmail.com>
2026-06-22 14:30:04 +0300

    feat(operate): export CommunicationsCenter component in index file

--- src/features/operate/index.ts
+++ src/features/operate/index.ts
@@ -3,3 +3,4 @@ export { CommandCenter } from "./command/components/command-center";
 export { RiskCenter } from "./risk/components/risk-center";
 export { EquipmentTracking } from "./equipment/components/equipment-tracking";
 export { WeatherAlertsCenter } from "./weather";
+export { CommunicationsCenter } from "./comms/components/communications-center";
